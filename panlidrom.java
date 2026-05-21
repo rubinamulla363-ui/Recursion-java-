f86688addc87a8ccca7bbccd3e08c9ce2d8cc882
rubinamulla363-ui <rubinamulla363@gmail.com>
2026-05-21 19:48:43 +0530

    Create panlidrom.java

---
+++ panlidrom.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Recursion {
+
+    static boolean isPalindrome(String str, int start, int end) {
+
+        if (start >= end) {
+            return true;
+        }
+
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        boolean result = isPalindrome(str, 0, str.length() - 1);
+
+        if (result) {
+            System.out.println("Palindrome String");
+        } else {
+            System.out.println("Not a Palindrome String");
+        }
+    }
+}
