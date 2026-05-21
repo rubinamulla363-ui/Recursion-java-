71358eb48b5af413b5dba73e907b6d2b35cedb02
rubinamulla363-ui <rubinamulla363@gmail.com>
2026-05-21 20:12:47 +0530

    Create Duplicate in array.java

---
+++ Duplicate in array.java
@@ -0,0 +1,21 @@
+public class DuplicateArray {
+
+    public static void main(String[] args) {
+
+        int arr[] = {1, 2, 3, 2, 4, 5, 1};
+
+        System.out.println("Duplicate elements are:");
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
